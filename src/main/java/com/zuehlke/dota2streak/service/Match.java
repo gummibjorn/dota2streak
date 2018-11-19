38ebeb7d10a9380431f68d23aeacd04d2102fdaa
Mario Egli <maeg@supermario.local>
2018-11-19 08:39:56 +0100

    Remove swagger generated opendota_api

---
+++ src/main/java/com/zuehlke/dota2streak/service/Match.java
@@ -0,0 +1,38 @@
+package com.zuehlke.dota2streak.service;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Match {
+    @JsonProperty("match_id")
+    private int matchId;
+    @JsonProperty("player_slot")
+    private int playerSlot;
+    @JsonProperty("radiant_win")
+    private boolean radiantWin;
+
+    public int getMatchId() {
+        return matchId;
+    }
+
+    public void setMatchId(int matchId) {
+        this.matchId = matchId;
+    }
+
+    public int getPlayerSlot() {
+        return playerSlot;
+    }
+
+    public void setPlayerSlot(int playerSlot) {
+        this.playerSlot = playerSlot;
+    }
+
+    public boolean isRadiantWin() {
+        return radiantWin;
+    }
+
+    public void setRadiantWin(boolean radiantWin) {
+        this.radiantWin = radiantWin;
+    }
+}
